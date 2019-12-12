abda3c13e3c22f1a1e28698613be957164a086cb
gmimg <gmimg9@gmail.com>
2019-12-12 11:17:29 +0900

    flask template rendering and variable

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
@@ -14,6 +14,30 @@ def hi():
 @app.route('/change')
 def change():
     return 'hello change'
+
+@app.route('/html_tag')
+def html_tag():
+    return '<h1>안녕하세요</h1>'
+
+@app.route('/html_file')
+def html_file():
+    return render_template('index.html')
+
+@app.route('/variable')
+def varialbe():
+    name = '지밍'
+    return render_template('variable.html', html_name=name)
+
+@app.route('/greeting/<string:name>/')
+def greeting(name):
+    def_name = name
+    return render_template('greeting.html', html_name = def_name)
+
+@app.route('/cube/<int:num>/')
+def cube(num):
+    num = num
+    sol = num*num*num
+    return render_template('cube.html', html_sol = sol, num = num)
 
 if __name__ == '__main__':
     app.run(debug=True)
